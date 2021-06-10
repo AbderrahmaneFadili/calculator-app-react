8e33be5cb631401ff855f10e78c92cab699f3d5e
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2021-06-10 12:25:42 +0100

    Change The ScreenInut To Screen Value

--- src/components/Screen/Screen.jsx
+++ src/components/Screen/Screen.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { ScreenWrapper, ScreenInput } from "./Screen.styles";
+import { ScreenWrapper, ScreenValue } from "./Screen.styles";
 
 const Screen = () => {
   const value = useSelector((state) => state.calculatorReducer.value);
   return (
     <ScreenWrapper>
-      <ScreenInput dir="rtl" onChange={() => null} value={value} />
+      <ScreenValue>{value}</ScreenValue>
     </ScreenWrapper>
   );
 };
--- src/components/Screen/Screen.styles.js
+++ src/components/Screen/Screen.styles.js
@@ -5,21 +5,19 @@ export const ScreenWrapper = styled.div`
   background-color: var(--screen-background);
   padding: 2.5rem 1.5rem;
   border-radius: 1rem;
+  display: flex;
+  justify-content: flex-end;
+  overflow: hidden;
 `;
 
-export const ScreenInput = styled.input`
-  width: 100%;
-  padding: 0;
+export const ScreenValue = styled.div`
   font-family: var(--main-font);
   font-size: 3rem;
-  border: 0;
-  outline: 0;
+  height: 60px;
   color: ${({ theme }) =>
     theme.theme === "theme3"
       ? "var(--text-yellow)"
       : theme.theme === "theme1"
       ? "var(--text-light)"
       : "var(--text-dark)"};
-  background-color: var(--screen-background);
-  direction: rtl;
 `;
